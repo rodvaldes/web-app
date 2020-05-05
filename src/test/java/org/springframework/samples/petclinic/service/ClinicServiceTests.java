82e4dc6e5891ca97bf4069ace2e5f970f3a544fe
Rodrigo Valdés <rodrigovaldes@gmail.com>
2020-05-05 04:46:39 -0400

    Corrige test con problemas.

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
@@ -172,7 +172,7 @@ public class ClinicServiceTests {
         Collection<Vet> vets = this.vets.findAll();
 
         Vet vet = EntityUtils.getById(vets, Vet.class, 3);
-        assertThat(vet.getLastName()).isEqualTo("Douglas");
+        assertThat(vet.getLastName()).isEqualTo("Casanova");
         assertThat(vet.getNrOfSpecialties()).isEqualTo(2);
         assertThat(vet.getSpecialties().get(0).getName()).isEqualTo("dentistry");
         assertThat(vet.getSpecialties().get(1).getName()).isEqualTo("surgery");
